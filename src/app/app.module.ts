c73c79a01d3449821f290a32dcf8b73d2c7360b0
José Antônio <jose.silva.br@outlook.com>
2017-05-21 16:29:22 -0300

    Add Connection Ionic serve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import {SignInPage} from "../pages/sign-in/sign-in";
 import {SignUpPage} from "../pages/sign-up/sign-up";
 import {TabsPage} from "../pages/tabs/tabs";
+import {AllCategoriesPage} from "../pages/all-categories/all-categories";
+
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': '7cdb35c1'
+  }
+};
 
 @NgModule({
   declarations: [
@@ -20,7 +28,8 @@ import {TabsPage} from "../pages/tabs/tabs";
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    CloudModule.forRoot(cloudSettings)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
