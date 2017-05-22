e385c3f5a96bf7df8e4e9823ea1b4e4e9ece9d5f
José Antônio <jose.silva.br@outlook.com>
2017-05-21 21:23:13 -0300

    Activity Category

---
+++ src/pages/category/category.ts
@@ -0,0 +1,74 @@
+import {Component, OnInit} from '@angular/core';
+import {AlertController, App, IonicPage, LoadingController, NavController, NavParams} from 'ionic-angular';
+import {Http} from "@angular/http";
+import {ProductDetailPage} from "../product-detail/product-detail";
+
+@IonicPage()
+@Component({
+  selector: 'page-category',
+  templateUrl: 'category.html',
+})
+export class CategoryPage implements OnInit {
+
+  nameCategory = "";
+  categoryProducts: Array<any>;
+
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private http: Http,
+              private loadingCtrl: LoadingController,
+              private alertCtrl: AlertController,
+              private app: App) {
+    this.nameCategory = navParams.get('name');
+  }
+
+  ngOnInit(): void {
+    this.getService();
+  }
+
+  getService() {
+    let loader = this.loadingCtrl.create({
+      content: "Carregando..."
+    });
+
+    loader.present();
+    let url = "http://localhost:8000/product/category/";
+    let idCategory = this.navParams.get('idCategory');
+    return this.http.get(url + idCategory).map(res => res.json()).subscribe(
+      data => {
+        if (!data) {
+          this.showAlert();
+          loader.dismiss();
+        } else {
+          this.categoryProducts = data;
+          loader.dismiss();
+        }
+      },
+      err => {
+        loader.dismiss();
+        this.showAlert();
+      }
+    );
+  }
+
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Atenção',
+      subTitle: 'Não foi possível acessar a internet',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  onClickProductDetail(id, name){
+    this.app.getRootNav().push(ProductDetailPage, {
+      idProduct: id,
+      name: name
+    })
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CategoryPage');
+  }
+}
