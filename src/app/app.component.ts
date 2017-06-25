d13245467d73932fd1c2b949a824641299d7946f
José Antônio <jose.silva.br@outlook.com>
2017-06-24 22:13:59 -0300

    isAuthenticated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,38 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Platform} from 'ionic-angular';
 import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
 
-import {HomePage} from '../pages/home/home';
 import {SignInPage} from "../pages/sign-in/sign-in";
 import {ServiceProvider} from "../providers/service/service";
 import {SharedCartServiceProvider} from "../providers/shared-cart-service/shared-cart-service";
+import {Auth} from "@ionic/cloud-angular";
+import {TabsPage} from "../pages/tabs/tabs";
 
 @Component({
   templateUrl: 'app.html',
   providers: [ServiceProvider, SharedCartServiceProvider]
 })
-export class MyApp {
+export class MyApp implements OnInit{
 
+  rootPage: any;
 
-  rootPage: any = SignInPage;
+  ngOnInit(): void {
+    if (this.auth.isAuthenticated()) {
+      this.rootPage = TabsPage;
+    }else{
+      this.rootPage = SignInPage;
+    }
+  }
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private auth: Auth) {
     platform.ready().then(() => {
 
       statusBar.styleDefault();
       splashScreen.hide();
     });
   }
+
 
   /*
    disconnectSubscription = this.network.onDisconnect().subscribe(() => {
